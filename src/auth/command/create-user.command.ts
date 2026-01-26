f09165ebfd9ff9657b6a462109084ef170897708
cochae <ycw1203@naver.com>
2026-01-26 14:36:30 +0900

    ♻️ Refactor: 비밀번호 null 허용 (소셜 회원가입 시)

--- src/auth/command/create-user.command.ts
+++ src/auth/command/create-user.command.ts
@@ -1,12 +1,12 @@
 export type CreateUserCommandProps = {
   email: string;
-  password: string;
+  password: string | null;
   nickname: string;
 };
 
 export class CreateUserCommand {
   readonly email!: string;
-  readonly password!: string;
+  readonly password!: string | null;
   readonly nickname!: string;
 
   constructor(props: CreateUserCommandProps) {
